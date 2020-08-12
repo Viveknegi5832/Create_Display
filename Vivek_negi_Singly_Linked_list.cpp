fac3502bdd1fc8cb2590a1696e02bfb16fd1c2e1
Viveknegi5832 <69526504+Viveknegi5832@users.noreply.github.com>
2020-08-12 16:15:29 +0530

    Add files via upload

---
+++ Vivek_negi_Singly_Linked_list.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+class SinglyLinkedList
+{
+    
+
+class Node
+{
+   public:
+    int data;
+    Node *next;
+};
+public:
+Node *head,*newnode,*temp;
+ 
+  void create()
+  {
+
+    
+    head=0;
+    
+    
+    int ch=1;
+    while(ch==1)
+    {
+        newnode = new Node();
+        cout<<"Enter the data: \n";
+        cin>>newnode->data;
+        newnode->next=0;
+        
+        if(head==0)
+        {
+            head=temp=newnode;
+        
+        }
+        else
+        {
+            temp->next=newnode;
+            temp=newnode;
+        }
+     cout<<"1 for continue and 0 for exit \n";
+     cin>>ch;
+        
+        
+    }
+    
+    
+    }
+    
+    void display()
+    {
+        
+    
+     temp=head;
+     while(temp!=0)
+     {
+         cout<<temp->data<<" ";
+         temp=temp->next;
+     }
+    
+}
+
+};
+
+int main()
+{
+    int n;
+    char ch;
+    SinglyLinkedList list;
+    
+    do{
+        
+        cout<<"enter 1 for create and 2 for display \n";
+        cin>>n;
+        
+        switch(n)
+        {
+            case 1:
+                   list.create();
+                   break;
+            case 2:
+                   list.display();
+                   break;
+            default:
+                   cout<<"wrong input \n";
+                   break;
+        }
+        
+        cout<<"y for continue \n";
+        cin>>ch;
+    }while(ch=='y');
+    
+    return 0;
+}
+
+
+
+
+
+
+
