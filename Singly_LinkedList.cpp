6b3a871751948dcce2483c9455e225c5dbb17776
Vivek Singh Negi <Singhvivek5832@gmail.com>
2020-08-21 20:52:44 +0530

    Update Singly_LinkedList.cpp

--- Singly_LinkedList.cpp
+++ Singly_LinkedList.cpp
@@ -4,8 +4,10 @@
 
 
 
-#include<iostream> 
-using namespace std;
+ #include<iostream> 
+ using namespace std;
+
+
  class SinglyLinkedList 
 { 
 class Node
@@ -18,12 +20,44 @@ class Node
     Node *head,*newnode,*temp,*temp1; 
 
 
+    SinglyLinkedList()
+    { 
+       head=NULL;
+    }
+
+   ~SinglyLinkedList()
+    {
+        temp=head;
+        cout<<" Deallocating memory....\n";
+        while(temp!=Null)
+          {
+           temp1=temp;
+           delete temp1;
+           temp=temp->next;
+           
+          }
+    }
+
+  void create();
+  void display();
+  void insert_at_beginning();
+  void insert_at_end();
+  void insert_in_between();
+  void delete_at_beginning();
+  void delete_at_end();
+  void delete_in_between();
+  void search();
+  void count();
+  void reverse();
+};
+
+
 
 /********CREATING_NODES*******/
 
 
 
-    void create()
+  void SinglyLinkedList :: create()
  { 
     head=NULL; 
     int ch=1;
@@ -51,21 +85,21 @@ class Node
 /*******DELETE_IN_BETWEEN*********/
 
 
-void delete_in_between()
+void SinglyLinkedList :: delete_in_between()
 {
-   int loc,i=1;
-   cout<<"enter the location of data you want to delete\n";
-   cin>>loc;
-   temp=head;
+     int loc,i=1;
+     cout<<"enter the location of data you want to delete\n";
+     cin>>loc;
+     temp=head;
    while(i<loc-1)
-{
-  temp=temp->next;
-  i++;
-  
-}temp1=temp->next;
-temp->next=temp->next->next;
-delete temp1;
-cout<<"Deleted in given location!\n";
+    {
+      temp=temp->next;
+      i++;
+    }
+    temp1=temp->next;
+    temp->next=temp->next->next;
+    delete temp1;
+    cout<<"Deleted in given location!\n";
 
 
 }
@@ -74,7 +108,7 @@ cout<<"Deleted in given location!\n";
 /*******DELETE_AT_BEGINNING******/
 
 
-void delete_at_beginning()
+void SinglyLinkedList :: delete_at_beginning()
 {
    temp=head;
    head=head->next;
@@ -87,25 +121,25 @@ void delete_at_beginning()
 /*******DELETE_AT_END********/
 
 
-void delete_at_end()
+void  SinglyLinkedList :: delete_at_end()
 
 {
-   temp=head;
-   while(temp->next!=newnode)
-  {
+     temp=head;
+     while(temp->next!=newnode)
+     {
        temp=temp->next;
-    }
-    delete temp->next;
-    temp->next=NULL;
-    newnode=temp;
-    cout<<"Deleted at end! \n";
-}
+     }
+      delete temp->next;
+      temp->next=NULL;
+      newnode=temp;
+      cout<<"Deleted at end! \n";
+} 
 
 
 /*******INSERT_AT_BEGINNING********/
 
 
-void insert_at_Beginning()
+void SinglyLinkedList :: insert_at_Beginning()
 
 {
   
@@ -122,127 +156,127 @@ void insert_at_Beginning()
 /*******INSERT_AT_END******/
 
 
-void insert_at_end()
+void SinglyLinkedList ::   insert_at_end()
 
-{
-  newnode = new Node();
-  cout<<"enter the data:";
-  cin>>newnode->data;
-  temp=head;
+{ 
+   newnode = new Node();
+   cout<<"enter the data:";
+   cin>>newnode->data;
+   temp=head;
   while(temp->next!=NULL)
-{
-   temp=temp->next;
-} 
- temp->next=newnode;
-newnode->next=NULL;
-cout<<"Inserted! \n";
+    {
+       temp=temp->next;
+    }  
+ 
+    temp->next=newnode;
+    newnode->next=NULL;
+    cout<<"Inserted! \n";
 }
 
 
 /*******INSERT_IN_BETWEEN******/
 
 
-void insert_in_between()
+void SinglyLinkedList :: insert_in_between()
 
 {
-  int location;
-  int i=1;
-  cout<<"enter the location you want the new node to be at:";
-  cin>>location;
-  newnode=new Node();
-  cout<<"enter the data:";
-  cin>>newnode->data;
+    int location;
+    int i=1;
+    cout<<"enter the location you want the new node to be at:";
+    cin>>location;
+    newnode=new Node();
+    cout<<"enter the data:";
+    cin>>newnode->data;
   
-  temp=head;
-  while(i<location-1)
-{
-  temp=temp->next;
-  i++;
-  
-}newnode->next=temp->next;
-temp->next=newnode;
-cout<<"Inserted! \n";
+   temp=head;
+   while(i<location-1)
+    {
+      temp=temp->next;
+      i++;
+    }
+    newnode->next=temp->next;
+    temp->next=newnode;
+    cout<<"Inserted! \n";
 
 }
 
 /********SEARCH_A_NODE******/
 
 
-void search()
+void  SinglyLinkedList :: search()
 {
   int n, count=0,flag=0;
   cout<<"Enter the value to be searched:";
   cin>>n;
   temp=head;
   while(temp!=NULL)
-{
-   ++count;
-
-  if(temp->data==n)
-{
-  flag=1;
-  break;
-
-}
-temp=temp->next;
-
-
-}
-if(flag==1)
-cout<<"data found at location:"<<count<<"\n";
-else
-cout<<"not found: \n";
+    {
+       ++count;
+   
+      if(temp->data==n)
+      {
+             flag=1;
+             break;
+      }
+         temp=temp->next;
+    }
+   if(flag==1)
+   cout<<"data found at location:"<<count<<"\n";
+   else
+   cout<<"not found: \n";
 }
 
 /*******COUNT_THE_NODES********/
 
 
-void count()
+void SinglyLinkedList :: count()
 {
- int count=0;
- temp=head;
+     int count=0;
+     temp=head;
  while(temp->next!=NULL)
-{
-  temp=temp->next;
-++count;
-}
-cout<<"no. of nodes ="<<count+1<<endl;
+   {
+      temp=temp->next;
+      ++count;
+   }
+   cout<<"no. of nodes ="<<count+1<<endl;
 
 }
 
 /******REVERSE_LINKEDLIST*******/
 
 
-void reverse()
+void SinglyLinkedList :: reverse()
 {
 
-  temp=head->next;
-  head->next=NULL;
-  newnode=head;
-  while(temp!=NULL)
-{
-   temp1=temp;
-   temp=temp->next;
-   temp1->next=head;
-   head=temp1;
-}
-  cout<<"Reversed! \n";
-  display();
+       temp=head->next;
+       head->next=NULL;
+       newnode=head;
+       while(temp!=NULL)
+    {
+       temp1=temp;
+       temp=temp->next;
+       temp1->next=head;
+       head=temp1;
+    }
+      cout<<"Reversed! \n";
+      display();
 
 }
 
 /*******DISPLAY_THE_NODES*********/
 
 
-void display() 
-{ temp=head; 
-while(temp!=NULL)
- { cout<<temp->data<<" ";
- temp=temp->next; 
-} 
-cout<<endl;
+void  SinglyLinkedList :: display() 
+{
+   temp=head; 
+   while(temp!=NULL)
+   { 
+      cout<<temp->data<<" ";
+      temp=temp->next; 
+   } 
+      cout<<endl;
 }
- }; 
+  
 int main() 
 { int n; 
 char ch;
@@ -273,7 +307,8 @@ case 6:
 case 7:
          list.delete_at_beginning();
          break;
-case 8:  list.delete_in_between();
+case 8:  
+         list.delete_in_between();
          break;
 case 9:  
          list.count();
@@ -287,7 +322,7 @@ case 11:
 case 12:
          exit(0); 
  }
- cout<<"want to continue (y/n) \n"; 
+ cout<<"want to continue (y/n) \n :"; 
 cin>>ch; 
 }while(ch=='y'); 
 return 0;
